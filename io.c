778a94579ced9204cd803cf496a7840595efabe9
plxxl2 <158720863+plxxl2@users.noreply.github.com>
2024-02-29 18:00:04 +0100

    comments to io.c

--- io.c
+++ io.c
@@ -3,7 +3,7 @@
 
 #include "io.h"
 
-void set_led(int input){
+void set_led(int input){ //allowed inputs 1-8, turns on that led
 	volatile int* leds = (volatile int*) 0xbf886110;
 	//PORTFSET = 7 - (input - 1);
 	
@@ -16,19 +16,19 @@ void set_led(int input){
 
 
 
-void reset_led(int input){
+void reset_led(int input){ //allowed inputs 1-8, turns off that led
 	//PORTFCLR = 7 - (input - 1);
 	volatile int* leds = (volatile int*) 0xbf886110;
 	*leds = *leds & (0xFFFFFFFF - (1 << (8 - input)));
 }
 
 
-void reset_led_all(void){
+void reset_led_all(void){ //turns of all leds
 	volatile int* leds = (volatile int*) 0xbf886110;
 	*leds = *leds & 0xFFFFFF00;
 }
 
-void set_led_all(void){
+void set_led_all(void){ //turns on all leds
 	volatile int* leds = (volatile int*) 0xbf886110;
 	*leds = *leds | 0x000000FF;
 	//PORTFSET = 0b11111111;
