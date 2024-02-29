8ae3a85f081dfb24b1e1c712b38c109ac24bbb78
plxxl2 <158720863+plxxl2@users.noreply.github.com>
2024-02-29 17:51:53 +0100

    Comments to misc.c

--- misc.c
+++ misc.c
@@ -3,7 +3,7 @@
 
 
 
-uint8_t reverse (uint8_t input){
+uint8_t reverse (uint8_t input){ //The bitmap in the display starts in upper left corner, contrary to "common" coordinates, this funcntion can reverse the array representing the bits of the display to allow 0,0 to be bottom left.
 	uint8_t output = 0;
 	int i;
 	for (i=0; i < 8; i++){
@@ -13,12 +13,12 @@ uint8_t reverse (uint8_t input){
 	return output;
 }
 
-int absolute(int a){
+int absolute(int a){ //gives absolute values for calculating vector distances between coordinates.
 	if (a < 0) return -a;
 	else return a;
 }
 
-double absoluted(double a){
+double absoluted(double a){ //gives absolute values for calculating vector distances between coordinates, but its a double
 	if (a < 0) return -a;
 	else return a;
 }
